545f5a6619e97b2b0ed157703b40f9db2fde80ec
lhuyz87 <lhuyz87@gmail.com>
2020-05-03 16:13:58 -0500

    @test

--- RimacProcuradorMobile/src/test/java/test/runner/RunYoursTest.java
+++ RimacProcuradorMobile/src/test/java/test/runner/RunYoursTest.java
@@ -1,11 +1,13 @@
 package test.runner;
 
 import org.junit.runner.RunWith;
+import org.testng.annotations.Test;
+
 import cucumber.api.CucumberOptions;
 import cucumber.api.junit.Cucumber;
 
 @RunWith(Cucumber.class)
-
+@Test
 @CucumberOptions(features = { "src/test/java/test/feature" }, tags = {"@ContactarCliente"}, glue = { "" })
 
 
