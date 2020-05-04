9a43553ef3bcc05e4f92cc8943627a1d7f3dfc5d
lhuyz87 <lhuyz87@gmail.com>
2020-05-04 01:42:21 -0500

    nombrar a tests

--- RimacProcuradorMobile/src/test/java/test/runner/RunYoursTests.java
+++ RimacProcuradorMobile/src/test/java/test/runner/RunYoursTests.java
@@ -6,13 +6,14 @@ import cucumber.api.CucumberOptions;
 import cucumber.api.junit.Cucumber;
 
 @RunWith(Cucumber.class)
+
 @org.testng.annotations.Test
 //@CucumberOptions(features = { "src/test/java/test/feature" }, tags = {"@RealizarLogin"}, glue = { "" })
 
 @CucumberOptions(features = { "src/test/java/test/feature" }, plugin= {"json:target/cucumber.json","html:target/size/cucumber-pretty"} ,tags = {"@RealizarLogin"}, glue = { "" })
 
 
-public class RunYoursTest 
+public class RunYoursTests 
 {
 // This class will be empty 
 }
